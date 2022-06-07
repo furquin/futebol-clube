9f96129fbd6620147eef311b31e224d2f598033e
furquin <laert.ff@gmail.com>
2022-06-06 22:17:19 -0300

    feat: implemneta filtros   para andamento da partida

--- app/backend/src/controller/Matches.ts
+++ app/backend/src/controller/Matches.ts
@@ -4,20 +4,14 @@ import MatchesService from '../service/Matches';
 export default class TeamController {
   static async getAll(req: Request, res: Response, next: NextFunction) {
     try {
+      const { inProgress } = req.query;
+      if (inProgress) {
+        const matches = await MatchesService.getByProgress(inProgress);
+
+        return res.status(200).json(matches);
+      }
       const matches = await MatchesService.getAll();
       return res.status(200).json(matches);
-    } catch (e) {
-      next(e);
-    }
-  }
-
-  static async getById(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { id } = req.params;
-
-      const teamId = await MatchesService.getByQuery(id);
-
-      return res.status(200).json(teamId);
     } catch (e) {
       next(e);
     }
--- app/backend/src/service/Matches.ts
+++ app/backend/src/service/Matches.ts
@@ -1,8 +1,9 @@
 import MatchesModel from '../database/models/matches';
 import TeamModel from '../database/models/teams';
+import IMatches from '../interface/IMatches';
 
 export default class TeamService {
-  static async getAll(): Promise<any> {
+  static async getAll(): Promise<IMatches[]> {
     const Matches = await MatchesModel.findAll({
       include: [
         { model: TeamModel,
@@ -21,8 +22,23 @@ export default class TeamService {
     return Matches;
   }
 
-  static async getByQuery(inProgress: number | string): Promise<any> {
-    const inProgressMatches = await MatchesModel.findAll({ where: { inProgress } });
+  static async getByProgress(inProgress: any): Promise<IMatches[]> {
+    let progress = 1;
+    if (inProgress !== 'true') { progress = 0; }
+
+    const inProgressMatches = await MatchesModel.findAll({ where: { inProgress: progress },
+      include: [
+        { model: TeamModel,
+          as: 'teamHome',
+          attributes: {
+            exclude: ['id'],
+          } },
+        { model: TeamModel,
+          as: 'teamAway',
+          attributes: {
+            exclude: ['id'],
+          } },
+      ] });
     return inProgressMatches;
   }
 }
